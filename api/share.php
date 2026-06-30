c43709def875664837bda734f9d5c71e49c17bc4
Markus Spitzner <177543817+acurioustale@users.noreply.github.com>
2026-06-30 18:52:26 +0100

    perf: enable database connection pooling

--- api/share.php
+++ api/share.php
@@ -444,6 +444,7 @@ function get_db_connection(): PDO
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES   => false,
+            PDO::ATTR_PERSISTENT         => true,
         ],
     );
 }
